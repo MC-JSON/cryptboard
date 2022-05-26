a6c32dccf55ccdec0fd4f97a2a79b754e7362b3b
Jason McCoy <jwmccoy29@gmail.com>
2022-05-26 10:42:19 -0500

    set up initial render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+//setting state variables
+//all available currency pairs on Coinbase
+const [currencies, setCurrencies] = useState([])
+//current pair selected by user
+const [pair, setPair] = useState('')
+//price of said currency
+const [price, setPrice] = useState('0.00')
+//historical price state
+const [pastData, setPastData] = useState({})
+
+//creating persistent websocket object
+const wsocket = useRef(null)
+//used to prevent an initial render
+let first = useRef(false)
+//base url for coinbase api
+const url = 'https://api.pro.coinbase.com'
+
+//API initial render
+useEffect(() => {
+  //connecting to websocket API
+  wsocket.current = new WebSocket('wss://ws-feed.pro.coinbase.com')
+  const apiCall = async () => {
+    await fetch(url + '/products')
+      .then((res) => res.json())
+      .then((data) => (pairs = data))
+
+    //filter to only USD based pairs
+    let filtered = pairs.filter((pair) => {
+      if (pair.quote_currency === 'USD') {
+        return pair
+      }
+    })
+    //sort filtered currency pairs alphabetically
+    filtered = filtered.sort((a, b) => {
+      if (a.base_currency < b.base_currency) {
+        return -1
+      }
+      if (a.base_currency > b.base_currency) {
+        return 1
+      }
+      return 0
+    })
+
+    setCurrencies(filtered)
+
+    first.current = true
+  }
+  apiCall()
+}, [])
 
 function App() {
   return (
@@ -19,7 +68,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
